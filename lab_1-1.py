1704af752b96c5e900c95509ed09ec51cb582836
Trevayne <p21twhyte@fairfieldprep.org>
2020-10-23 10:11:33 -0400

    lab1-1 done

---
+++ lab_1-1.py
@@ -0,0 +1,17 @@
+#Author Tow 10/23/2020
+
+#int(a)
+#name error
+
+#int('a')
+#value error
+
+
+#'a' + 2
+#type error
+
+#import date
+#module not found error
+
+#print( "I'm a happy camper!)
+#syntax error
